b7deb7b2c9c72f182fcdcd87f05eb17b64b24abc
guptaraghav29 <2001guptar@gmail.com>
2019-11-09 20:10:48 -0800

    Added Comments, Improved Styling

--- DiningHall.js
+++ DiningHall.js
@@ -1,16 +1,24 @@
 describe('Dining Hall Menu', function()
 {
-    // var countAI;
-    // var countLO;
-    // var countGM;
     var AI = {count : 0};
     var LO = {count : 0};
     var GM = {count : 0};
 
+    //prints out current working directory:
     console.log('Current directory: ' + process.cwd());
 
+    /*
+        Protractor goes to the website of the dining halls and then stores 
+        the number of vegeterian items into a variable. Then it will later 
+        compare the value with other dining halls and output the dining hall 
+        with the highest number of vegeterian options. 
+    */
+
+    
     beforeAll( function() 
     {
+        console.log("");
+
         //LO
         browser.manage().deleteAllCookies();
         browser.waitForAngularEnabled(false);
@@ -45,21 +53,32 @@ describe('Dining Hall Menu', function()
         });    
     })
 
-    it("Test", function() {
+    /*
+        This function compares the dining halls number of vegeterian options.
+    */
+   
+    it("Check Which Menu Has Most options", function() {
         if((AI.count > GM.count) && (AI.count > LO.count))
         {
+            console.log("");
             console.log("A-I has the most vegeterian options today!");
+            console.log("Please go to A-I");
         }
         else if((GM.count > AI.count) && (GM.count > LO.count))
         {
+            console.log("");
             console.log("Glen Mor has the most vegeterian options today!");
+            console.log("Please go to Glen Mor");
         }
         else if((LO.count > GM.count) && (LO.count > AI.count))
         {
+            console.log("");
             console.log("Lothian has the most vegeterian options today!");
+            console.log("Please go to Lothian");
         }
         else
         {
+            console.log("");
             console.log("No good options anywhere today!");
         }
 
--- config.js
+++ config.js
@@ -2,10 +2,6 @@ exports.config =
     {
         //defines which specs Protractor runs
         specs: ['DiningHall.js'],
-        /*
-            Use './specs/**.js' if you want to run all the scripts
-        */
-
 
         //which browsers Protractor will run tests on
         capabilities:
